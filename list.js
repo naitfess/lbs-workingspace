981618e8b88fabeec92dbebfc6ea6638bdc9e47b
naitfess <naitfess@gmail.com>
2024-12-02 23:50:34 +0700

    r

---
+++ list.js
@@ -0,0 +1,22 @@
+const coWorkingSpace = [
+    { name: "CoHive Yogyakarta", address: "Jl. Ringroad Selatan No. 163", lat: -7.7732, lon: 110.3730 },
+    { name: "Go-Tix Coworking Space", address: "Jl. Jenderal Sudirman No. 25", lat: -7.7943, lon: 110.3671 },
+    { name: "Alamanda Coworking", address: "Jl. Siliwangi No. 45", lat: -7.7861, lon: 110.3582 },
+    { name: "Stark Innovate", address: "Jl. Dr. Sutomo No. 14", lat: -7.7584, lon: 110.3740 },
+    { name: "WORKHUB Yogyakarta", address: "Jl. Malioboro No. 103", lat: -7.7972, lon: 110.3653 },
+    { name: "Mekar Space", address: "Jl. Timoho No. 6", lat: -7.7525, lon: 110.3600 },
+    { name: "Spaces Yogyakarta", address: "Jl. Magelang KM 6", lat: -7.7817, lon: 110.3961 },
+    { name: "The Foundry", address: "Jl. Pahlawan No. 2", lat: -7.7603, lon: 110.3925 },
+    { name: "Katalis Coworking", address: "Jl. Cempaka No. 12", lat: -7.7580, lon: 110.3994 },
+    { name: "Workspaces YK", address: "Jl. Seturan No. 56", lat: -7.7750, lon: 110.3965 },
+    { name: "Bilik Coworking", address: "Jl. Diponegoro No. 60", lat: -7.7792, lon: 110.3571 },
+    { name: "WeWork Yogyakarta", address: "Jl. Pahlawan No. 3", lat: -7.7541, lon: 110.3728 },
+    { name: "Katalis Space", address: "Jl. Jendral Sudirman No. 10", lat: -7.7624, lon: 110.3663 },
+    { name: "Indie Space", address: "Jl. Gajah Mada No. 45", lat: -7.7873, lon: 110.3867 },
+    { name: "Venture Space", address: "Jl. Prawirotaman No. 16", lat: -7.7569, lon: 110.3647 },
+    { name: "Coworking Hub YK", address: "Jl. Solo No. 8", lat: -7.7819, lon: 110.3973 },
+    { name: "Elevate Coworking", address: "Jl. Magelang No. 20", lat: -7.7845, lon: 110.3992 },
+    { name: "The Lab", address: "Jl. Timoho No. 5", lat: -7.7531, lon: 110.3669 },
+    { name: "CreativeSpace YK", address: "Jl. Seturan No. 12", lat: -7.7783, lon: 110.3897 },
+    { name: "Muda Coworking", address: "Jl. Sriwedari No. 7", lat: -7.7790, lon: 110.3784 }
+];
